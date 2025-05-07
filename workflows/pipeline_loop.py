4c8d54f062fa814c3829d662af79d5a3d8493d39
nuuuwan <nuuuwan@gmail.com>
2025-05-07 08:11:38 +0530

    new data

--- workflows/pipeline_loop.py
+++ workflows/pipeline_loop.py
@@ -1,15 +1,21 @@
 import time
 import os
+from utils import Log
 from workflows.scrape_results import main as scrape_results_main
 from workflows.write_reports import main as write_reports_main
+
+log = Log("pipeline_loop")
+T_SLEEP = 30
 
 
 def main():
     while True:
-        os.cmd("git pull origin main")
+        log.info("-" * 40)
+        log.info("pipeline_loop")
+        os.system("git pull origin main")
         scrape_results_main()
         write_reports_main()
-        time.sleep(30)
+
         for cmd in [
             "git add data/*",
             "git add README.md",
@@ -17,6 +23,8 @@ def main():
             "git push origin main",
         ]:
             os.system(cmd)
+        log.debug(f"😴 {T_SLEEP}s")
+        time.sleep(T_SLEEP)
 
 
 if __name__ == "__main__":
