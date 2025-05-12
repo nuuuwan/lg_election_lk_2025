a25904ebf5a8ba4d5a946edd1c80c2bafef9dba8
nuuuwan <nuuuwan@gmail.com>
2025-05-12 15:18:23 +0530

    itak

--- workflows/analysis/hexmaps/election_2025/p_itak/__main__.py
+++ workflows/analysis/hexmaps/election_2025/p_itak/__main__.py
@@ -2,10 +2,10 @@ from workflows.analysis.common import get_code_to_result
 from workflows.analysis.hexmaps.lg_types.__main__ import build_hexmap
 from workflows.analysis.hexmaps.election_2025.votes.__main__ import (
     NO_ELECTION,
-    get_color,
 )
 from utils import Log
 import os
+from utils_future import Color
 
 DID_NOT_CONTEST = "Did Not Contest"
 
@@ -21,7 +21,14 @@ def get_label_for_percentage(p_turnout):
     return f"{p_lower:.0%} to {p_upper:.0%}"
 
 
-def get_legend_label(ent):
+def get_mid_percentage_from_label(label):
+    p_lower, p_upper = label.split(" to ")
+    p_lower = float(p_lower[:-1]) / 100.0
+    p_upper = float(p_upper[:-1]) / 100.0
+    return (p_lower + p_upper) / 2.0
+
+
+def get_legend_label(ent, display_party_code):
     result = code_to_result.get(ent.code, None)
     if result is None:
         return NO_ELECTION
@@ -29,38 +36,46 @@ def get_legend_label(ent):
     total_votes = result["summary_data"]["valid"]
     for party_result in result["party_result_data_list"]:
         party_code = party_result["party_code"]
-        if party_code == "ITAK":
+        if party_code == display_party_code:
             npp_votes = party_result["votes"]
             return get_label_for_percentage(npp_votes / total_votes)
 
     return DID_NOT_CONTEST
 
 
-def get_color(legend_label):
+def get_color(legend_label, display_party_hue):
     if legend_label == NO_ELECTION:
         return "#000"
 
     if legend_label == DID_NOT_CONTEST:
         return "#eee"
 
-    return {
-        get_label_for_percentage(0.65): "#800",
-        get_label_for_percentage(0.55): "#f00",
-        get_label_for_percentage(0.45): "#f80",
-        get_label_for_percentage(0.35): "#ff0",
-        get_label_for_percentage(0.25): "#0f0",
-        get_label_for_percentage(0.15): "#0c0",
-        get_label_for_percentage(0.05): "#080",
-    }.get(legend_label, "#fff")
+    p = get_mid_percentage_from_label(legend_label)
+
+    hue = display_party_hue
+    saturation = 100
+    MIN_LIGHTNESS = 10
+    MAX_LIGHTNESS = 90
+    lightness = MIN_LIGHTNESS + (1 - p) * (MAX_LIGHTNESS - MIN_LIGHTNESS)
+
+    return Color.from_hls(
+        hue,
+        lightness,
+        saturation,
+    ).hex
+
+
+def build_hexmap_for_party(display_party_code, display_party_hue):
+    build_hexmap(
+        f"% for {display_party_code}",
+        lambda ent: get_legend_label(ent, display_party_code),
+        lambda label: get_color(label, display_party_hue),
+        os.path.dirname(__file__),
+    )
 
 
 def main():
-    build_hexmap(
-        "% for ITAK",
-        get_legend_label,
-        get_color,
-        os.path.dirname(__file__),
-    )
+    build_hexmap_for_party("ITAK", 45)
 
 
 if __name__ == "__main__":
