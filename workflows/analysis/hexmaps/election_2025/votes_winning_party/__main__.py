abcd3c2036ea84efa3035d39725afb4f0b802222
nuuuwan <nuuuwan@gmail.com>
2025-05-12 19:19:53 +0530

    Vote % for Winning Party

--- workflows/analysis/hexmaps/election_2025/votes_winning_party/__main__.py
+++ workflows/analysis/hexmaps/election_2025/votes_winning_party/__main__.py
@@ -13,9 +13,14 @@ code_to_result = get_code_to_result()
 
 log = Log("seats")
 
+Q = 0.1
 
-def get_party_color(winning_party_code):
-    return "#ccc"
+
+def get_legend_label_from_p(p):
+
+    lower = int(p / Q) * Q
+    upper = lower + Q
+    return f"[{lower:.0%}, {upper:.0%})"
 
 
 def get_legend_label(ent):
@@ -25,14 +30,12 @@ def get_legend_label(ent):
         return NO_ELECTION
 
     party_result_data_list = result["party_result_data_list"]
-    limit_5pct = result["summary_data"]["valid"] * 0.25
-    at_least_one_seat = sum(
-        [
-            1 if result["votes"] > limit_5pct else 0
-            for result in party_result_data_list
-        ]
-    )
-    return str(at_least_one_seat)
+    party_result_data_list.sort(key=lambda x: x["votes"], reverse=True)
+    winning_party_result = party_result_data_list[0]
+    winning_party_votes = winning_party_result["votes"]
+    total_votes = result["summary_data"]["valid"]
+    p_winning_party = winning_party_votes / total_votes
+    return get_legend_label_from_p(p_winning_party)
 
 
 def get_color(legend_label):
@@ -40,15 +43,35 @@ def get_color(legend_label):
     if legend_label == NO_ELECTION:
         return "#000"
 
-    p_scaled = ((int(legend_label)) - 0) / (3 - 0)
-    hue = p_scaled * 210
-    color = Color.from_hls(hue, 75, 100).hex
-    return color
+    return "white"
+
+
+def get_mid_percentage_for_label(label):
+    if label == NO_ELECTION:
+        return 0.0
+
+    p_lower, p_upper = label.split(", ")
+    p_lower = float(p_lower[1:-1])
+    p_upper = float(p_upper[:-2])
+    return (p_lower + p_upper) / 2.0 / 100.0
+
+
+def get_color(legend_label):
+    if legend_label == NO_ELECTION:
+        return "#000"
+
+    mid_p = get_mid_percentage_for_label(legend_label)
+    MIN_P, MAX_P = 0 + Q, 0.7 - Q
+    p_scaled = (mid_p - MIN_P) / (MAX_P - MIN_P)
+    hue = (1 - p_scaled) * 210
+    sat = 100
+    light = 50
+    return Color.from_hls(hue, light, sat).hex
 
 
 def main():
     build_hexmap(
-        "Parties with at least 25% votes",
+        "Vote % for Winning Party",
         get_legend_label,
         get_color,
         os.path.dirname(__file__),
